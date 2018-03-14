184073476932ff7d337203eb899303c3655d86e7
sshah98 <shahsuraj261@gmail.com>
2018-03-14 14:25:16 -0400

    edited files and now this works

--- registration.py
+++ registration.py
@@ -1,85 +1,79 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import getpass
 
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver import ChromeOptions, Chrome
-
-
-
-baseurl = 'https://banweb.gwu.edu/PRODCartridge/twbkwbis.P_WWWLogin'
+from selenium.webdriver.common.keys import Keys
 
 class Registration(object):
 
-	def __init__(self, login, password, secondpass):
-		self.login = login
-		self.password = password
-		self.secondpass = secondpass
+    def __init__(self, login, password, secondpass):
+        self.login = login
+        self.password = password
+        self.secondpass = secondpass
 
-	def loginToAccount(self):
-		options = Options()
-		options.add_argument('--disable-gpu')
-		options.add_argument("--disable-extensions")
-		options.add_experimental_option("detach", True)
-		mydriver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', chrome_options=options)
-		# mydriver = webdriver.Firefox()
-		mydriver.get(baseurl)
-
-		# print("Logging in...")
-		mydriver.find_element_by_id("UserID").send_keys(self.login)
-		mydriver.find_element_by_xpath('//*[@id="PIN"]/input').send_keys(self.password)
-		mydriver.find_element_by_xpath("//p//input[@type='submit']").click()
-
-
-
-		mydriver.find_element_by_id('answer').send_keys(self.secondpass)
-
-		mydriver.find_element_by_xpath("//p//input[@type='submit']").click()
-
-		mydriver.get('https://banweb.gwu.edu/PRODCartridge/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu')
-
-		# time.sleep(2)
-
-		mydriver.get('https://banweb.gwu.edu/PRODCartridge/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu')
-
-		mydriver.get('https://banweb.gwu.edu/PRODCartridge/bwskfreg.P_AddDropCrse')
+    def loginToAccount(self):
 		
-		mydriver.find_element_by_xpath("//form//input[@type='submit']").click()
+		baseurl = 'https://banweb.gwu.edu/PRODCartridge/twbkwbis.P_WWWLogin'
+        options = Options()
+        options.add_argument('--disable-gpu')
+        options.add_argument("--disable-extensions")
+        options.add_experimental_option("detach", True)
+        mydriver = webdriver.Chrome(
+            executable_path='chromedriver', chrome_options=options)
+        mydriver.get(baseurl)
 
-		
+        mydriver.find_element_by_id("UserID").send_keys(self.login)
+        mydriver.find_element_by_xpath(
+            '//*[@id="PIN"]/input').send_keys(self.password)
+        mydriver.find_element_by_xpath("//p//input[@type='submit']").click()
 
+        mydriver.find_element_by_id('answer').send_keys(self.secondpass)
+
+        mydriver.find_element_by_xpath("//p//input[@type='submit']").click()
+
+        mydriver.get(
+            'https://banweb.gwu.edu/PRODCartridge/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu')
+
+        time.sleep(1)
+
+        mydriver.get(
+            'https://banweb.gwu.edu/PRODCartridge/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu')
+
+        time.sleep(1)
+
+        mydriver.find_element_by_link_text(
+            'Register, Drop and/or Add Classes').click()
+
+        time.sleep(4)
+
+        print('Please click submit and wait while CRNS get populated')
+
+        mydriver.find_element_by_id("crn_id1").send_keys(crn1)
+        mydriver.send_keys(Keys.TAB)
+
+        mydriver.find_element_by_id("crn_id2").send_keys(crn2)
+        mydriver.send_keys(Keys.TAB)
+
+        mydriver.find_element_by_id("crn_id3").send_keys(crn3)
+        mydriver.send_keys(Keys.TAB)
+
+        mydriver.find_element_by_id("crn_id4").send_keys(crn4)
+        mydriver.send_keys(Keys.TAB)
+
+        mydriver.find_element_by_id("crn_id5").send_keys(crn5)
+        mydriver.send_keys(Keys.TAB)
+
+
+crn1 = '111111'
+crn2 = '222222'
+crn3 = '333333'
+crn4 = '444444'
+crn5 = '555555'
 
 myuser = input('Enter GWID: ')
 mypass = getpass.getpass('Enter password: ')
-mypass2 = getpass.getpass('Enter Mothers Maiden Name: ')
+mypass2 = getpass.getpass('Enter Second Password: ')
 
 myobj = Registration(myuser, mypass, mypass2)
 myobj.loginToAccount()
-
-
-
-
-# crn1 = driver.find_element_by_id("crn_id1")
-# crn1.send_keys('351231')
-# crn1.send_keys(Keys.TAB)
-
-# crn2 = driver.find_element_by_id("crn_id2")
-# crn2.send_keys('351231')
-# crn2.send_keys(Keys.TAB)
-
-
-# crn3 = driver.find_element_by_id("crn_id3")
-# crn3.send_keys('351231')
-# crn3.send_keys(Keys.TAB)
-
-# crn4 = driver.find_element_by_id("crn_id4")
-# crn4.send_keys('351231')
-# crn4.send_keys(Keys.TAB)
-
-
-# crn5 = driver.find_element_by_id("crn_id5")
-# crn5.send_keys('351231')
-# crn5.send_keys(Keys.TAB)
